d1b9f14c419bacd88ee31417f5c9bc2d4dd425a2
Raji Sarafadeen Sanjo <omosanjos77@gmail.com>
2022-07-31 15:21:16 +0100

    changed to HashRouter from BrowserRouter

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {ChakraProvider} from '@chakra-ui/react'
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
       <ChakraProvider>
-        <BrowserRouter>
+        <HashRouter hashType="hashbang">
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </ChakraProvider>
   </React.StrictMode>
 );
