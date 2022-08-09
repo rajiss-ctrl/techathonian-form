1974556cf95f4277a0e97f12bca1cc826c8c4ac7
Raji Sarafadeen Sanjo <omosanjos77@gmail.com>
2022-08-09 18:46:33 +0100

    added additional random api call to dashboard

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React,{useState,useEffect} from 'react'
-import {Flex,Box,Button,Heading, VStack,Text} from '@chakra-ui/react'
+import {Flex,Box,Button,Heading, VStack,Text, Image} from '@chakra-ui/react'
 import {  useNavigate } from 'react-router-dom'
 
 
@@ -7,12 +7,23 @@ import {  useNavigate } from 'react-router-dom'
 const Dashboard = () => {
   const navigate = useNavigate()
   const [userDisplaDetails,setUserDisplaDetails] = useState([])
+  const [apiData, setApiData] = useState([])
+  const url = 'https://random-data-api.com/api/restaurant/random_restaurant?size=8'
+  const restaurant = async () =>{
+      const res = await fetch(url);
+      const data = await res.json();
+      setApiData(data)
+  }
+
+  useEffect(()=>{
+     restaurant()
+},[])
   useEffect(()=>{
      const user = JSON.parse(sessionStorage.getItem('user'))
      setUserDisplaDetails(user)
-  
-  
 },[])
+
+console.log(apiData)
   return (
     <Box width={'100%'} padding={{base:'60px', md:'30px'}}>
 
@@ -29,7 +40,21 @@ const Dashboard = () => {
   
 </VStack>
 
-<Flex justifyContent={'center'} >
+
+          <Heading textAlign={'center'} fontSize={'24px'}>TECHATHON RESTAURANT</Heading>
+<Flex  flexWrap={'wrap'}>
+  {apiData.map((item)=>{
+    return (
+ <Box key={item.id} padding={'20px'} width={'300px'}>
+  <Heading >{item.name}</Heading>
+  <Text >Address:-{item.address}</Text> 
+  <Image src={item.logo} width={'200px'} alt='restaurant'/>
+  <Heading fontSize={'16px'}>Phone: {item.phone_number}</Heading>
+ </Box>
+    )
+  })}
+  </Flex>
+      <Flex justifyContent={'right'} >
   <Button 
           background={'blue'}
           color={'white'} 
@@ -39,6 +64,7 @@ const Dashboard = () => {
           navigate('/')
           }}>Log Out</Button>
 </Flex>
+
     </Box>
   )
 }
